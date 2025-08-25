1bac16ccb5318045327b15a068b9c48f4c3516f7
David Di Giorgio <ddigiorg@gmail.com>
2025-08-24 21:33:38 -0400

    ecs updates

--- src/core/ecs.hpp
+++ src/core/ecs.hpp
@@ -4,6 +4,7 @@
 
 #include <algorithm> // for std::sort
 #include <array>
+#include <cstddef> // for std::byte
 #include <cstdint>
 #include <iostream>
 #include <limits> // for std::numeric_limits
@@ -42,7 +43,7 @@ struct EntityRecord {
 };
 
 // =============================================================================
-// Component Types
+// Component
 // =============================================================================
 
 #define cidof(C) getComponentId<C>()
@@ -64,68 +65,140 @@ inline id_t getComponentId() {
 }
 
 struct ComponentRecord {
-    id_t   id;
-    size_t size;
-    size_t offset;
-
-    void clear() {
-        id     = INVALID_ID;
-        size   = 0;
-        offset = 0;
-    }
+    id_t   componentId;
+    size_t elementSize;
+    size_t arrayOffset;
+    size_t arraySize;
 };
 
 // =============================================================================
-// Chunk Types
+// Chunk
 // =============================================================================
 
 // A Chunk represents a contiguous block of memory for storing entity component 
 // data.  It is designed for efficient memory access and L1 cache locality.
-using ChunkData = char;
 
-// TODO: I guess I dont need the header anymore since Im using ChunkRecord to store chunk metadata
-// 16 kilobytes minus 256 byte header
-// constexpr const size_t CHUNK_DATA_SIZE = 16 * 1024 - 256;
-constexpr const size_t CHUNK_DATA_SIZE = 16 * 1024;
+class Chunk {
+public:
+    Chunk(); // TODO
 
-struct ChunkArrayRecord {
-    void*  addr;
-    size_t size;
+    // TODO
+    uint32_t addEntity(); // returns new entity index
+    void removeEntity(uint32_t index); // swap-pop for dense arrays
+    void copyEntityTo(uint32_t index, Chunk& dst, uint32_t dstIndex);
+
+    template <typename C>
+    bool hasComponent() const;
+
+    template <typename C>
+    C* getArray();
+
+    template <typename C>
+    const C* getArray() const;
+
+    template <typename C>
+    C& getElement(uint32_t index);
+
+    template <typename C>
+    const C& getElement(uint32_t index) const;
+
+    template <typename C>
+    void setElement(uint32_t index, const C& value);
+
+    bool isFull() const { return entityCount >= entityCapacity; }
+
+    void incrementVersion() { ++version; }
+    size_t getEntityCount() const { return entityCount; }
+    size_t getCapacity() const { return entityCapacity; }
+    Archetype* getArchetype() const { return archetype; }
+
+    static constexpr size_t TOTAL_SIZE  = 16 * 1024;
+    static constexpr size_t HEADER_SIZE = 128;
+    static constexpr size_t BUFFER_SIZE = TOTAL_SIZE - HEADER_SIZE;
+
+private:
+    // --- Header (metadata, 256 bytes) ---
+    uint32_t entityCount;       // num entities currently in this chunk
+    uint32_t entityCapacity;    // max entities for this chunk
+    uint32_t version;           // structural change version
+    Archetype* archetype;       // pointer to archetype definition
+    void* sharedComponentArray; // pointer to shared component data
+
+    // --- Data buffer (aligned packed storage, 16 KB - 256 B) ---
+    alignas(64) std::array<std::byte, BUFFER_SIZE> buffer;
 };
 
-struct ChunkRecord {
-    idx_t  chunkIdx;
-    id_t   archetypeId;
-    size_t entityCount;
-    size_t entityCapacity;
-    std::array<ChunkArrayRecord, COMPONENT_CAPACITY> cidToArray;
-};
+template <typename C>
+bool Chunk::hasComponent() const {
+    return archetype->hasComponent(getComponentId<C>());
+}
+
+template <typename C>
+C* Chunk::getArray() {
+    ASSERT(archetype != nullptr, "Chunk has no archetype set.");
+    size_t offset = archetype->getOffset(getComponentId<C>());
+    ASSERT(offset + sizeof(C) * entityCount <= BUFFER_SIZE,
+           "Component array exceeds chunk buffer.");
+    return reinterpret_cast<C*>(buffer.data() + offset);
+}
+
+template <typename C>
+const C* Chunk::getArray() const {
+    ASSERT(archetype != nullptr, "Chunk has no archetype set.");
+    size_t offset = archetype->getOffset(getComponentId<C>());
+    ASSERT(offset + sizeof(C) * entityCount <= BUFFER_SIZE,
+           "Component array exceeds chunk buffer.");
+    return reinterpret_cast<const C*>(buffer.data() + offset);
+}
+
+template <typename C>
+C& Chunk::getElement(uint32_t index) {
+    ASSERT(index < entityCount, "Index out of bounds.");
+    return getArray<C>()[index];
+}
+
+template <typename C>
+const C& Chunk::getElement(uint32_t index) const {
+    ASSERT(index < entityCount, "Index out of bounds.");
+    return getArray<C>()[index];
+}
+
+template <typename C>
+void setElement(uint32_t index, const C& value) {
+    ASSERT(index < entityCount, "Index out of bounds.");
+    getArray<C>()[index] = value;
+}
 
 // =============================================================================
-// Archetype Types
+// Archetype
 // =============================================================================
 
-struct ArchetypeNeighbors {
-    id_t componentId;
-    id_t left;
-    id_t right;
-};
+class Archetype {
+public:
+    // TODO: functions
 
-struct ArchetypeRecord {
+private:
+    static id_t count;
+
     id_t   id;
     mask_t mask;
-    size_t entityCapacity;
-    std::vector<ComponentRecord> components;
-    std::array<ArchetypeNeighbors, COMPONENT_CAPACITY> neighbors;
+    idx_t  chunkOpenIdx;
+    idx_t  chunkEntityCapacity;
+    std::array<size_t, COMPONENT_CAPACITY> idToOffset;
+    std::array<id_t,   COMPONENT_CAPACITY> leftArchetypes;
+    std::array<id_t,   COMPONENT_CAPACITY> rightArchetypes;
+
+    std::vector<Chunk*> chunks;
 };
 
 // =============================================================================
-// Manager
+// Manager Class
 // =============================================================================
 
 class Manager {
 public:
     Manager();
+    ~Manager();
 
     // entity functions
     bool hasEntity(id_t entityId) const;
@@ -168,22 +241,22 @@ private:
 
     // archetype functions
     id_t _getOrCreateArchetype(const std::vector<id_t>& componentIds);
+    void _pushChunkOnArchetype(id_t archetypeId);
+    void _popChunkOffArchetype(id_t archetypeId);
+    void _freeChunks();
 
     // entity data
     std::vector<EntityRecord> entities{};
-    std::vector<id_t> freeEntityIds{};
-    id_t nextEntityId{0};
+    std::vector<id_t> eFreeIds{};
+    id_t eNextId{0};
 
     // component data
     std::array<ComponentRecord, COMPONENT_CAPACITY> components{};
-    id_t componentCount{0};
+    id_t cCount{0};
 
     // archetype data
-    std::unordered_map<mask_t, id_t> maskToId;
-    std::vector<ArchetypeRecord> archetypes;
-    std::vector<std::vector<ChunkRecord>> chunkMeta;
-    std::vector<std::vector<ChunkData*>>  chunkData;
-    id_t nextId{0};
+    std::vector<Archetype> archetypes;
+    std::unordered_map<mask_t, id_t> archetypeMaskToId;
 
     // EventManager eventMgr;
     // QueryManager queryMgr;
@@ -191,17 +264,24 @@ private:
 };
 
 // =============================================================================
-// Constructor
+// Constructor and Destructor
 // =============================================================================
 
 Manager::Manager() {
-    // TODO: need this?
-    for (auto& component : components) {
-        component.clear();
+
+    for (auto& c : components) {
+        c.componentId = INVALID_ID;
+        c.elementSize = 0;
+        c.arrayOffset = 0;
+        c.arraySize   = 0;
     }
 
     // create default archetype with no components
     _getOrCreateArchetype({});
+}
+
+Manager::~Manager() {
+    _freeChunks();
 }
 
 // =============================================================================
@@ -214,14 +294,22 @@ bool Manager::hasEntity(id_t entityId) const {
 }
 
 id_t Manager::createEntity(const std::vector<id_t>& componentIds) {
-    id_t entityId = _nextEntity();
-    id_t archetypeId = _getOrCreateArchetype(componentIds);
+    id_t eId = _nextEntity();
+    id_t aId = _getOrCreateArchetype(componentIds);
+    std::vector<ChunkMeta >& chunkMeta = aChunkMeta[aId];
+    std::vector<ChunkData*>& chunkData = aChunkData[aId];
+
+    // allocate new chunk if no chunks exist in archetype
+    if (chunkData.empty()) {
+        _pushChunkOnArchetype(aId);
+    }
+
     // TODO: archetype and chunk stuff here !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    return entityId;
+    return eId;
 }
 
 void Manager::removeEntity(id_t entityId) {
-    ASSERT(entityId < nextEntityId, "EntityId does not exist.");
+    ASSERT(entityId < eNextId, "EntityId " << entityId << " does not exist.");
     // TODO: archetype and chunk stuff here !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     _freeEntity(entityId);
 }
@@ -238,14 +326,14 @@ void Manager::removeEntity(id_t entityId) {
 id_t Manager::_nextEntity() {
     id_t entityId = INVALID_ID;
 
-    if (!freeEntityIds.empty()) {
-        entityId = freeEntityIds.back();
-        freeEntityIds.pop_back();
+    if (!eFreeIds.empty()) {
+        entityId = eFreeIds.back();
+        eFreeIds.pop_back();
         entities[entityId].entityId = entityId;
         return entityId;
     }
 
-    entityId = nextEntityId++;
+    entityId = eNextId++;
     // TODO: is this optimal?
     EntityRecord entity;
     entity.clear();
@@ -255,10 +343,10 @@ id_t Manager::_nextEntity() {
 }
 
 void Manager::_freeEntity(id_t entityId) {
-    ASSERT(entityId < nextEntityId, "EntityId does not exist.");
+    ASSERT(entityId < eNextId, "EntityId " << entityId << " does not exist.");
     // TODO: need to check duplicate ids
     entities[entityId].clear();
-    freeEntityIds.push_back(entityId);
+    eFreeIds.push_back(entityId);
 }
 
 // =============================================================================
@@ -271,7 +359,7 @@ bool Manager::hasComponent() const {
 }
 
 bool Manager::hasComponent(id_t componentId) const {
-    return componentId < componentCount;
+    return componentId < cCount;
 }
 
 template <typename C>
@@ -280,28 +368,44 @@ bool Manager::isTag() const {
 }
 
 bool Manager::isTag(id_t componentId) const {
-    return components[componentId].size == 0;
+    return components[componentId].elementSize == 0;
 }
 
 template <typename C>
 id_t Manager::registerComponent() {
     static_assert(std::is_base_of<Component, C>::value, "Component must inherit from Component base.");
-    ASSERT(componentCount < COMPONENT_CAPACITY, "Component registry full.");
+    ASSERT(cCount < COMPONENT_CAPACITY, "Component registry full.");
+
     id_t id = getComponentId<C>();
     ASSERT(!hasComponent(id), "Component \"" << typeid(C).name() << "\" already exists.");
-    components[id] = { id, sizeof(C), 0 };
-    componentCount++;
+
+    components[id] = {
+        /*componentId=*/ id,
+        /*elementSize=*/ sizeof(C),
+        /*arrayOffset=*/ 0,
+        /*  arraySize=*/ 0,
+    };
+
+    cCount++;
     return id;
 }
 
 template <typename C>
 id_t Manager::registerTag() {
     static_assert(std::is_base_of<Tag, C>::value, "Tag must inherit from Tag base.");
-    ASSERT(componentCount < COMPONENT_CAPACITY, "Component registry full.");
+    ASSERT(cCount < COMPONENT_CAPACITY, "Component registry full.");
+
     id_t id = getComponentId<C>();
     ASSERT(!hasComponent(id), "Tag \"" << typeid(C).name() << "\" already exists.");
-    components[id] = { id, 0, 0 };
-    componentCount++;
+
+    components[id] = {
+        /*componentId=*/ id,
+        /*elementSize=*/ 0,
+        /*arrayOffset=*/ 0,
+        /*  arraySize=*/ 0,
+    };
+
+    cCount++;
     return id;
 }
 
@@ -316,49 +420,106 @@ id_t Manager::_getOrCreateArchetype(const std::vector<id_t>& componentIds) {
     std::sort(sortedIds.begin(), sortedIds.end());
 
     // build mask
-    mask_t mask = 0;
+    mask_t aMask = 0;
     for (id_t cId : sortedIds) {
-        mask |= (mask_t(1) << cId);
+        aMask |= (mask_t(1) << cId);
     }
 
     // check if archetype already exists and return it
-    auto it = maskToId.find(mask);
-    if (it != maskToId.end()) {
+    auto it = aMaskToId.find(aMask);
+    if (it != aMaskToId.end()) {
         return it->second;
     }
 
     // allocate new archetype
-    size_t size = 0;
-    id_t id = nextId++;
-    ArchetypeRecord archetype{ id, mask, 0, {}, {} };
+    id_t aId = aNextId++;
+    ArchetypeRecord archetype {
+        /*   archetypeId=*/ aId,
+        /* archetypeMask=*/ aMask,
+        /*  openChunkIdx=*/ 0,
+        /*entityCapacity=*/ 0,
+        /*    components=*/ {},
+        /*     neighbors=*/ {},
+    };
     archetype.components.reserve(sortedIds.size() + 1);
 
     // add entity id component
+    size_t aSize = 0;
     size_t idSize = sizeof(id_t);
     archetype.components.push_back({ INVALID_ID, idSize, 0});
-    size += idSize;
+    aSize += idSize;
 
     // gather component metadata
     for (id_t cId : sortedIds) {
-        const ComponentRecord& component = components[cId];
-        archetype.components.push_back({ component.id, component.size, 0 });
-        size += component.size;
+        const ComponentRecord& c = components[cId];
+        archetype.components.push_back(
+            ComponentRecord {
+                /*componentId=*/ c.componentId,
+                /*elementSize=*/ c.elementSize,
+                /*arrayOffset=*/ 0,
+                /*  arraySize=*/ 0, 
+            }
+        );
+        aSize += c.elementSize;
     }
 
-    ASSERT(size < CHUNK_DATA_SIZE, "Archetype size exceeds chunk capacity");
+    ASSERT(aSize < CHUNK_DATA_SIZE, "Archetype size exceeds chunk capacity");
 
-    archetype.entityCapacity = (size > 0) ? CHUNK_DATA_SIZE / size : CHUNK_DATA_SIZE;
+    archetype.entityCapacity = (aSize > 0) ? CHUNK_DATA_SIZE / aSize : CHUNK_DATA_SIZE;
 
     // assign component offsets
     size_t offset = 0;
-    for (auto& archetypeComponent : archetype.components) {
-        archetypeComponent.offset = offset;
-        offset += archetype.entityCapacity * archetypeComponent.size;
+    size_t size = 0;
+    for (auto& ac : archetype.components) {
+        ac.arrayOffset = offset;
+        ac.arraySize = ac.elementSize * archetype.entityCapacity;
+        offset += ac.arraySize;
     }
 
+    // append new archetype
+    aMaskToId[aMask] = aId;
     archetypes.push_back(std::move(archetype));
-    maskToId[mask] = id;
-    return id;
+    aChunkMeta.push_back({});
+    aChunkData.push_back({});
+
+    return aId;
+}
+
+void Manager::_pushChunkOnArchetype(id_t archetypeId) {
+    ASSERT(archetypeId < archetypes.size(), "Archetype " << archetypeId << " does not exist.");
+    ArchetypeRecord& aRecord = archetypes[archetypeId];
+
+    ChunkMeta cMeta {
+        /*   archetypeId=*/ archetypeId,
+        /*      chunkIdx=*/ (idx_t)aChunkData[archetypeId].size(),
+        /*   entityCount=*/ 0,
+        /*entityCapacity=*/ aRecord.entityCapacity,
+        /*    cidToArray=*/ {},
+    };
+
+    ChunkData* cData = static_cast<ChunkData*>(malloc(CHUNK_DATA_SIZE));
+
+    for (const auto& c : aRecord.components) {
+        if (c.componentId == INVALID_ID) continue;
+        ChunkArrayRecord& ca = cMeta.cidToArray[c.componentId];
+        ca.addr = reinterpret_cast<void*>(reinterpret_cast<std::byte*>(cData) + c.arrayOffset);
+        ca.size = c.arraySize;
+    }
+
+    aChunkMeta[archetypeId].push_back(cMeta);
+    aChunkData[archetypeId].push_back(cData);
+}
+
+void Manager::_popChunkOffArchetype(id_t archetypeId) {
+
+}
+
+void Manager::_freeChunks() {
+    for (auto& chunks : aChunkData) {
+        for (auto chunk : chunks) {
+            free(chunk);
+        }
+    }
 }
 
 // =============================================================================
@@ -373,34 +534,36 @@ void Manager::print() {
 
 void Manager::printEntities() {
     std::cout << "entities:" << std::endl;
-    for (const auto& entity : entities) {
-        std::cout << "  - entityId: "    << entity.entityId    << std::endl;
-        std::cout << "  - archetypeId: " << entity.archetypeId << std::endl;
-        std::cout << "  - chunkIdx: "    << entity.chunkIdx    << std::endl;
-        std::cout << "  - entityIdx: "   << entity.entityIdx   << std::endl;
+    for (const auto& e : entities) {
+        std::cout << "  - entityId: "    << e.entityId    << std::endl;
+        std::cout << "  - archetypeId: " << e.archetypeId << std::endl;
+        std::cout << "  - chunkIdx: "    << e.chunkIdx    << std::endl;
+        std::cout << "  - entityIdx: "   << e.entityIdx   << std::endl;
     }
 }
 
 void Manager::printComponents() {
     std::cout << "components:" << std::endl;
-    for (id_t id = 0; id < componentCount; id++) {
-        const ComponentRecord& component = components[id];
-        std::cout << "  - id: "   << component.id   << std::endl;
-        std::cout << "    size: " << component.size << std::endl;
+    for (id_t id = 0; id < cCount; id++) {
+        const ComponentRecord& c = components[id];
+        std::cout << "  - componentId: " << c.componentId << std::endl;
+        std::cout << "    elementSize: " << c.elementSize << std::endl;
     }
 }
 
 void Manager::printArchetypes() {
     std::cout << "archetypes:" << std::endl;
-    for (const auto& archetype : archetypes) {
-        std::cout << "  - id: "             << archetype.id             << std::endl;
-        std::cout << "    mask: "           << archetype.mask           << std::endl;
-        std::cout << "    entityCapacity: " << archetype.entityCapacity << std::endl;
-        std::cout << "    components: "                                 << std::endl;
-        for (const auto& component : archetype.components) {
-            std::cout << "    - id: "     << component.id     << std::endl;
-            std::cout << "      size: "   << component.size   << std::endl;
-            std::cout << "      offset: " << component.offset << std::endl;
+    for (const auto& a : archetypes) {
+        std::cout << "  - archetypeId: "    << a.archetypeId    << std::endl;
+        std::cout << "    archetypeMask: "  << a.archetypeMask  << std::endl;
+        std::cout << "    openChunkIdx: "   << a.openChunkIdx   << std::endl;
+        std::cout << "    entityCapacity: " << a.entityCapacity << std::endl;
+        std::cout << "    components: "                         << std::endl;
+        for (const auto& c : a.components) {
+            std::cout << "    - componentId: " << c.componentId << std::endl;
+            std::cout << "      elementSize: " << c.elementSize << std::endl;
+            std::cout << "      arrayOffset: " << c.arrayOffset << std::endl;
+            std::cout << "      arraySize: "   << c.arraySize   << std::endl;
         }
     }
 }
